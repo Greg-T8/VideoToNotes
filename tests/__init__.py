723ec47552025a91c4e6fe3b9e3c4737d2e14dba
Greg Tate <gregt8@gmail.com>
2026-01-25 07:32:07 -0600

    Remove test files for assemble, extract, and merge stages from the Exam Notes Generator test suite.

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# -------------------------------------------------------------------------
-# File: __init__.py
-# Description: Tests package initialization
-# Context: Exam Notes Generator - test suite
-# Author: Greg Tate
-# -------------------------------------------------------------------------
-
-"""Test suite for Exam Notes Generator."""
